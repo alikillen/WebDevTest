3a53ae5fc246c09befa304f9eda8af593de86c31
alikillen <alisoneme1994@gmail.com>
2021-04-11 01:35:23 +1000

    navbar work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 import BurgerButton from "../components/BurgerButton"
+import { useMediaQuery } from "../helpers/MediaQueries";
 import "../App.css"
 import "../styles/Navbar.css"
 
 // import button
 
 
+
+
+
 function Navbar() {
 
+
+  let isPageDesktop = useMediaQuery("(min-width: 800px)");
+  let isPageMobile = useMediaQuery("(max-width: 800px)");
 
 
   const [showLinks, setShowLinks] = useState(false);
@@ -16,28 +23,46 @@ function Navbar() {
   return (
     <div className='Navbar'>
       <div className='leftSide'>
-        <div className='links' id={showLinks ? "hidden" : ""}>
-          <a href='/'>Home</a>
-          <a href='https://hismileteeth.com/products/teeth-whitening-kit'>
+        <div className={isPageDesktop ? "links-desktop" : 'links' } id={showLinks ? "hidden" : ""}>
+          {/* {isPageMobile && (
+            <a className='home-link' href='/'>
+              Home
+            </a>
+          )} */}
+          <a
+            className='shop-kit-link'
+            href='https://hismileteeth.com/products/teeth-whitening-kit'
+          >
             Shop the Kit
           </a>
-          <a href='https://hismileteeth.com/pages/products'>
+          <a
+            className='explore-range-link'
+            href='https://hismileteeth.com/pages/products'
+          >
             Explore the Range
           </a>
-          <a href='https://hismileteeth.com/cart'>My Cart</a>
+          {/* <a className='cart-link' href='https://hismileteeth.com/cart'>
+            My Cart
+          </a> */}
         </div>
 
-
-        <button onClick={() => setShowLinks(!showLinks)}>
+        <button
+          onClick={() => setShowLinks(!showLinks)}
+          className='burger-button'
+        >
           {/* Open */}
           <BurgerButton />
           {/* className="burger-button-component" id={showLinks ? ".change" : ""} */}
         </button>
       </div>
 
-      {/* <div className="rightSide"> */}
-      {/* button here when mobile */}
-      {/* </div> */}
+      <h1 className={isPageDesktop ? "nav-title-desktop" : "nav-title-mobile"}>Hismile</h1>
+
+      <div className='rightSide'>
+        <div className='links'>
+          <a href='https://hismileteeth.com/cart'>My Cart</a>
+        </div>
+      </div>
     </div>
   );
 }
