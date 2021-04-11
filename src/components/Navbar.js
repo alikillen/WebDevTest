6aa5a649b3586aca0d8d4bc727c4b385201fd6a2
alikillen <alisoneme1994@gmail.com>
2021-04-11 11:45:28 +1000

    menu breakpoint fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,22 @@
-import React, { useState } from 'react'
-import BurgerButton from "../components/BurgerButton"
+import React, { useState } from "react";
+import BurgerButton from "../components/BurgerButton";
 import { useMediaQuery } from "../helpers/MediaQueries";
-import "../App.css"
-import "../styles/Navbar.css"
-
-// import button
-
-
-
-
+import "../App.css";
+import "../styles/Navbar.css";
 
 function Navbar() {
-
-
-  let isPageDesktop = useMediaQuery("(min-width: 800px)");
-  let isPageMobile = useMediaQuery("(max-width: 800px)");
-
+  let isPageDesktop = useMediaQuery("(min-width: 880px)");
+  let isPageMobile = useMediaQuery("(max-width: 880px)");
 
   const [showLinks, setShowLinks] = useState(false);
-
 
   return (
     <div className='Navbar'>
       <div className='leftSide'>
-        <div className={isPageDesktop ? "links-desktop" : 'links' } id={showLinks ? "hidden" : ""}>
+        <div
+          className={isPageDesktop ? "links-desktop" : "links"}
+          id={showLinks ? "hidden" : ""}
+        >
           {/* {isPageMobile && (
             <a className='home-link' href='/'>
               Home
@@ -46,17 +39,19 @@ function Navbar() {
           </a> */}
         </div>
 
-        <button
-          onClick={() => setShowLinks(!showLinks)}
-          className='burger-button'
-        >
-          {/* Open */}
-          <BurgerButton />
-          {/* className="burger-button-component" id={showLinks ? ".change" : ""} */}
-        </button>
+        
+          <button
+            onClick={() => setShowLinks(!showLinks)}
+            className='burger-button'
+          >
+            <BurgerButton />
+          </button>
+    
       </div>
 
-      <h1 className={isPageDesktop ? "nav-title-desktop" : "nav-title-mobile"}>Hismile</h1>
+      <h1 className={isPageDesktop ? "nav-title-desktop" : "nav-title-mobile"}>
+        Hismile
+      </h1>
 
       <div className='rightSide'>
         <div className='links'>
@@ -67,4 +62,4 @@ function Navbar() {
   );
 }
 
-export default Navbar
+export default Navbar;
